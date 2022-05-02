0a2f5d78af41232310f2bb44c523ff32a4d10931
py4econ@gmail.com <Py4Econmn>
2022-05-02 19:45:20 +0200

    rocplot two

--- roc_draw.py
+++ roc_draw.py
@@ -1,23 +1,36 @@
 
 import pandas as pd
+import numpy as np
 from sklearn import metrics
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('roc_raw.csv')
+def plot_roc(y1,y2):
+    fpr1, tpr1, thresholds = metrics.roc_curve(df['crisisJST'], df[y1])
+    fpr2, tpr2, thresholds = metrics.roc_curve(df['crisisJST'], df[y2])
+    auc1 = metrics.roc_auc_score(df['crisisJST'], df[y1])
+    auc2 = metrics.roc_auc_score(df['crisisJST'], df[y2])
+    plt.figure()
+    lw = 2
+    plt.plot(fpr1,tpr1,label=y1+ " " + str(np.round(auc1,2)))
+    plt.plot(fpr2,tpr2,label=y2+ " " + str(np.round(auc2,2)))
+    plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Receiver operating characteristic curve")
+    plt.legend(loc="lower right")
+    plt.show()
+
+# 1.3
+df = pd.read_csv('roc_raw_in_out.csv')
 df.dropna(inplace=True)
+plot_roc("yhat_base_insample","yhat_base_outsample")
 
-fpr, tpr, thresholds = metrics.roc_curve(df['crisisJST'], df['yhat1'])
-fpr2, tpr2, thresholds = metrics.roc_curve(df['crisisJST'], df['yhat2'])
+# 1.4
+df = pd.read_csv('roc_raw_all_in.csv')
+df.dropna(inplace=True)
+plot_roc("yhat_insample","yhat_outsample")
 
-plt.figure()
-lw = 2
-plt.plot(fpr,tpr)
-plt.plot(fpr2,tpr2)
-plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel("False Positive Rate")
-plt.ylabel("True Positive Rate")
-plt.title("Receiver operating characteristic example")
-plt.legend(loc="lower right")
-plt.show()
+narrowm eq_capgain housing_capgain bond_tr bill_rate
+
